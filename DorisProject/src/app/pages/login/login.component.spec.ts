823db17c36f2718904893a82e0dfb86dd26cbbd4
NillsonCastillo <nillson.castillo@alumnos.ipleones.cl>
2022-03-30 10:10:42 -0300

    login reactivo

--- DorisProject/src/app/pages/login/login.component.spec.ts
+++ DorisProject/src/app/pages/login/login.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  const formBuilder: FormBuilder=new FormBuilder();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [FormsModule, Validators, ReactiveFormsModule]
     })
     .compileComponents();
   }));
@@ -22,4 +25,30 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('should detect form is valid',()=>{
+    fixture.nativeElement.querySelector('button').click();
+    expect (component.login()).toEqual('invalid_form');
+  });
+
+  it('should validate correct user and password',()=>{
+    component.loginForm=formBuilder.group({
+      rut:'9999999-9',
+      contrasena:'123456'
+    });
+    fixture.nativeElement.querySelector('button').click();
+    expect(component.login()).toEqual('login_valid');
+  });
+
+  it('should deny access with incorrect password',()=>{
+    component.loginForm=formBuilder.group({
+      rut:'9999999-9',
+      contrasena:'123'
+    });
+    fixture.nativeElement.querySelector('button').click();
+    expect(component.login()).toEqual('login_invalid');
+  });
+
+
 });
--- DorisProject/src/app/pages/login/login.component.ts
+++ DorisProject/src/app/pages/login/login.component.ts
@@ -1,34 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm: FormGroup;
 
-usuario={
-  rut:'',
-  contrasena:''
-}
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      rut: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(10)]],
+      contrasena: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
 
 
-entrar( forma:NgForm){
+  get rutValid(){
+    return this.loginForm.get('rut').invalid&&this.loginForm.get('rut').touched
+  }
 
-if (forma.invalid) {
-  Object.values(forma.controls).forEach(control=>{
-    control.markAllAsTouched();
-  })
+  get passwordValid(){
+    return this.loginForm.get('contrasena').invalid&&this.loginForm.get('contrasena').touched
+  }
 
-}
-
-  
-}
-
+  login() {
+    let userLogged = 'invalid_form';
+    console.log('Valores del form --> ', this.loginForm.value);
+    if(this.loginForm.valid) {
+      if (this.loginForm.value.rut === '9999999-9' && this.loginForm.value.contrasena === '123456') {
+        userLogged = 'login_valid';
+      } else {
+        userLogged = 'login_invalid';
+      }
+      console.log('Respuesta del servicio de login --> ', userLogged);
+    }
+    if (this.loginForm.invalid) {
+      Object.values(this.loginForm.controls).forEach(control=>{
+        control.markAllAsTouched();
+      })
+    return userLogged;
+  }
+  }
 }
