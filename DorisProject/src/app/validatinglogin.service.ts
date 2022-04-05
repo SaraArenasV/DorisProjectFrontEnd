45bdd7100f008e46964de6d069e39c84edd5cf57
mae256 <ismael256@gmail.com>
2022-04-05 16:25:43 -0400

    add enviroment vars

--- DorisProject/src/app/validatinglogin.service.ts
+++ DorisProject/src/app/validatinglogin.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,8 @@ export class ValidatingloginService {
 
 
   login(user: any): Observable<any> {
-    return this.http.post('https://dorisprojectbackend-dev.herokuapp.com/api/v1/userValidation', user);
+    const url = `${environment.apiDoris}${environment.login}`;
+    return this.http.post(url, user);
   }
 
 
