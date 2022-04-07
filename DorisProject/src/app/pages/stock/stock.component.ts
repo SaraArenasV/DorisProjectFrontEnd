fa9a328947b458bbb0281d74712023135d829ab8
mae256 <ismael256@gmail.com>
2022-04-07 17:24:58 -0400

    add style and new ms for list products

--- DorisProject/src/app/pages/stock/stock.component.ts
+++ DorisProject/src/app/pages/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { ProductService } from 'src/app/service/product.service';
 
 
 @Component({
@@ -9,10 +10,58 @@ import {Component, OnInit} from '@angular/core';
 export class StockComponent implements OnInit {
 
   products: any[] = [];
-  constructor() { }
+  productsMaster: any[] = [];
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products= [{ID:"1",Sku:"555555",Description:"Salt",Brand:"Mae",Category:"Home",Stock:"10",UpdateDate:"1/01/2022"}];
+    this.productService.getSotck().subscribe(data =>{
+      data.forEach(product => {
+        let dataproduct ={ID:product.id,Sku:product.sku,Description:product.description,Brand:product.brand,Category:product.categoryName,Stock:product.stock,UpdateDate:product.outgress}
+        this.productsMaster.push(dataproduct);
+      });
+      this.products=this.productsMaster;
+    });
+    //this.products= [{ID:"1",Sku:"555555",Description:"Salt",Brand:"Mae",Category:"Home",Stock:"10",UpdateDate:"1/01/2022"}];
   }
+
+  search(event: any) {
+    const SearchList =[];
+    if (event.target.value == "")
+        this.products= this.productsMaster;
+      else
+      {
+          this.productsMaster.forEach(product =>{
+            console.log(product)
+            if(product.ID.toString().toLowerCase().includes(event.target.value.toLowerCase()))
+            {
+              SearchList.push(product);
+              return;
+            }
+            if(product.Description.toLowerCase().includes(event.target.value.toLowerCase()) )
+            {
+              SearchList.push(product);
+              return;
+            }
+            if(product.Sku.toLowerCase().includes(event.target.value.toLowerCase()) )
+            {
+              SearchList.push(product);
+              return;
+            }
+            if(product.Category.toLowerCase().includes(event.target.value.toLowerCase()) )
+            {
+              SearchList.push(product);
+            }
+
+          });
+          this.products=SearchList;
+      }
+     
+      
+  }
+
+  searchQuery(columnValue, value): boolean {
+    return columnValue.toLowerCase().includes(value.toLowerCase());
+  }
+
 
 }
--- DorisProject/src/environments/environment.ts
+++ DorisProject/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiDoris:'https://dorisprojectbackend-dev.herokuapp.com',
-  getStock:'/api/getproductlist',
+  getStock:'/api/getproductsCategory',
   login:'/api/v1/userValidation'
 
 };
