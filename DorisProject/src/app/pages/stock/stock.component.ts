e776936d9566bbd60c3d9ab3f5d7c3a7fe2348a3
Ruben Barrera <rbarrera@bmsoluciones.cl>
2022-04-08 10:15:45 -0400

    merge

--- DorisProject/src/app/pages/stock/stock.component.ts
+++ DorisProject/src/app/pages/stock/stock.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { ProductService } from 'src/app/service/product.service';
+import {ProductService} from 'src/app/service/product.service';
 import {Router} from '@angular/router';
 
 
@@ -12,50 +12,55 @@ export class StockComponent implements OnInit {
 
   products: any[] = [];
   productsMaster: any[] = [];
-  constructor(private productService: ProductService,   private  router: Router) { }
+
+  constructor(private productService: ProductService, private  router: Router) {
+  }
 
   ngOnInit(): void {
-    this.productService.getSotck().subscribe(data =>{
+    this.productService.getSotck().subscribe(data => {
       data.forEach(product => {
-        let dataproduct ={ID:product.id,Sku:product.sku,Description:product.description,Brand:product.brand,Category:product.categoryName,Stock:product.stock,UpdateDate:product.outgress}
+        let dataproduct = {
+          ID: product.id,
+          Sku: product.sku,
+          Description: product.description,
+          Brand: product.brand,
+          Category: product.categoryName,
+          Stock: product.stock,
+          UpdateDate: product.outgress
+        };
         this.productsMaster.push(dataproduct);
       });
-      this.products=this.productsMaster;
+      this.products = this.productsMaster;
     });
     //this.products= [{ID:"1",Sku:"555555",Description:"Salt",Brand:"Mae",Category:"Home",Stock:"10",UpdateDate:"1/01/2022"}];
   }
 
   search(event: any) {
-    const SearchList =[];
-    if (event.target.value == "")
-        this.products= this.productsMaster;
-      else
-      {
-          this.productsMaster.forEach(product =>{
-            console.log(product)
-            if(product.ID.toString().toLowerCase().includes(event.target.value.toLowerCase()))
-            {
-              SearchList.push(product);
-              return;
-            }
-            if(product.Description.toLowerCase().includes(event.target.value.toLowerCase()) )
-            {
-              SearchList.push(product);
-              return;
-            }
-            if(product.Sku.toLowerCase().includes(event.target.value.toLowerCase()) )
-            {
-              SearchList.push(product);
-              return;
-            }
-            if(product.Category.toLowerCase().includes(event.target.value.toLowerCase()) )
-            {
-              SearchList.push(product);
-            }
+    const SearchList = [];
+    if (event.target.value == '') {
+      this.products = this.productsMaster;
+    } else {
+      this.productsMaster.forEach(product => {
+        console.log(product);
+        if (product.ID.toString().toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(product);
+          return;
+        }
+        if (product.Description.toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(product);
+          return;
+        }
+        if (product.Sku.toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(product);
+          return;
+        }
+        if (product.Category.toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(product);
+        }
 
-          });
-          this.products=SearchList;
-      }
+      });
+      this.products = SearchList;
+    }
 
 
   }
