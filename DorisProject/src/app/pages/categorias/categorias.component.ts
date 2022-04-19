5ee06b518401dcb6c76b21b6f9bcf51f20522efc
vanevegas <vanessamvegas@gmail.com>
2022-04-19 12:09:30 -0400

    changes in delete categories

--- DorisProject/src/app/pages/categorias/categorias.component.ts
+++ DorisProject/src/app/pages/categorias/categorias.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoriasService } from 'src/app/service/categorias.service';
-import {Router} from '@angular/router';
-import {ModalComponent} from '../modal/modal.component';
-import {MatDialog} from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { ModalComponent } from '../modal/modal.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-categorias',
@@ -13,13 +13,13 @@ import {MatDialog} from '@angular/material/dialog';
 export class CategoriasComponent implements OnInit {
 
   forma: FormGroup;
-  categorias: any[]= [];
-  categoriasMaster : any []=[];
+  categorias: any[] = [];
+  categoriasMaster: any[] = [];
   responseModal: any;
 
-  constructor(private fb: FormBuilder, private categoryService: CategoriasService, private  router: Router, private dialog: MatDialog) {
+  constructor(private fb: FormBuilder, private categoryService: CategoriasService, private router: Router, private dialog: MatDialog) {
 
- 
+
   }
 
   ngOnInit(): void {
@@ -30,16 +30,16 @@ export class CategoriasComponent implements OnInit {
   getCategorias() {
     this.categoryService.getCategory().subscribe((data) => {
       this.categorias = data;
-      this.categoriasMaster=data;
+      this.categoriasMaster = data;
     }, err => { console.log("error ", err) }
     );
   }
 
 
 
-addCategory(){
-  this.router.navigate(['addcategory']);
-}
+  addCategory() {
+    this.router.navigate(['addcategory']);
+  }
 
   get category() {
     return this.forma.get('categorias') as FormArray;
@@ -54,53 +54,64 @@ addCategory(){
   }
 
   search(event: any) {
-    const SearchList =[];
+    const SearchList = [];
     if (event.target.value == "")
-        this.categorias= this.categoriasMaster;
-      else
-      {
-          this.categoriasMaster.forEach(category =>{
-            console.log(category)
-          if(category.description.toLowerCase().includes(event.target.value.toLowerCase()) )
-            {
-              SearchList.push(category);
-              return;
-            }
-            if(category.name.toLowerCase().includes(event.target.value.toLowerCase()) )
-            {
-              SearchList.push(category);
-              return;
-            }
-          });
-          this.categorias=SearchList;
-      }
-     
-    
-      
+      this.categorias = this.categoriasMaster;
+    else {
+      this.categoriasMaster.forEach(category => {
+        console.log(category)
+        if (category.description.toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(category);
+          return;
+        }
+        if (category.name.toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(category);
+          return;
+        }
+        if (category.id.toString().toLowerCase().includes(event.target.value.toLowerCase())) {
+          SearchList.push(category);
+          return;
+        }
+      });
+      this.categorias = SearchList;
+    }
+
+
+
   }
 
-  delete(category: any){
+  delete(category: any) {
     const categoryNew = {
       id: category.id,
-      name: category.name   
+      name: category.name
 
     };
     this.categoryService.delete(categoryNew).subscribe((data) => {
       console.log(data)
-    }, err => { console.log("error ", err) }
+      if (data.success == true) {       
+        this.openModal('succesDelete', '');
+      } else {
+        if (data.message == ' : Category has Product assigned, it is not possible to delete '){
+          this.openModal('errorDeleteCategory', 'This category has products, it cannot be deleted');
+        }else{
+          this.openModal('errorDeleteCategory', "It's have ocurred an error, please try later ");
+        }
+      }
+    }, err => {
+      this.openModal('errorDeleteCategory', "It's have ocurred an error, please try later ");
+    }
     );
   }
 
-  openModal(request: string, category: any) {
+  openModal(request: string, message : string) {
     const dialogRef = this.dialog.open(ModalComponent, {
-      width: '445px', height: '235px',
-      data: {textrequest: request, textresponse: this.responseModal,category:category}
+      data: { textrequest: request, textresponse: this.responseModal, nameService: 'Category', message: message }
     });
     dialogRef.afterClosed().subscribe(result => {
       this.responseModal = result;
       switch (this.responseModal) {
         case 'delete':
-          this.delete(category);
+
           break;
 
       }
@@ -108,18 +119,5 @@ addCategory(){
     });
   }
 
-  openModalMsge(request: string) {
-    const dialogRef = this.dialog.open(ModalComponent, {
-      data: {textrequest: request, textresponse: this.responseModal, nameService:'Product'}});
-    dialogRef.afterClosed().subscribe(result => {
-      this.responseModal = result;
-      switch (this.responseModal) {
-        case 'save':
-         
-          break;
-
-      }
-    });
-  }
 
 }
