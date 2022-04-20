f9ff8990ee1a7dd0315a62be1c4522e05b893207
vanevegas <vanessamvegas@gmail.com>
2022-04-20 11:30:42 -0400

    banner

---
+++ DorisProject/src/app/pages/banner/banner.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+
+  slides:any;
+
+  constructor() { 
+   this.slides = [
+      {'image': 'assets/img/logo.png'}, 
+      {'image': 'assets/img/loupe.png'},
+      {'image': 'assets/img/search.png'}, 
+    ];
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
