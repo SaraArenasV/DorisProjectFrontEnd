818055bcfdc020023ccdd17b181cf8dd0059b7a2
DESKTOP-OOHTFCE\SII Group <nillson.castillo@siigroup.cl>
2022-04-21 11:45:46 -0400

    Botones landing con colores ok.

--- DorisProject/src/app/pages/header/header.component.ts
+++ DorisProject/src/app/pages/header/header.component.ts
@@ -8,18 +8,24 @@ import {Router} from '@angular/router';
 })
 
 export class HeaderComponent implements OnInit {
-
+  product: boolean = true;
+  categories: boolean = true;
   name = window.localStorage.getItem('username');
+
   constructor(private  router: Router ) {
      }
 
   ngOnInit(): void {}
 
   gotoProducts() {
+    this.product = false;
+    this.categories = true;
     this.router.navigate(['stock']);
-  }
 
+  }
   gotoCategorys() {
+    this.product = true;
+    this.categories = false;
     this.router.navigate(['categorias']);
   }
 
