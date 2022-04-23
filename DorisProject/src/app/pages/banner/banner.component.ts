d2dbb01b371aa3cca6209b2d4303a5a52fd07551
Ruben Barrera <rbarrera@bmsoluciones.cl>
2022-04-22 21:54:43 -0400

    fix carrousel style its responsive
    se arregla el carruselse deja responsivo con iamgenes fijas que no se mueven para nada y  responsivo

--- DorisProject/src/app/pages/banner/banner.component.ts
+++ DorisProject/src/app/pages/banner/banner.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { MatCarousel, MatCarouselComponent } from '@ngmodule/material-carousel';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
 
   slides:any;
 
-  constructor() { 
+  constructor() {
    this.slides = [
-      {'image': 'assets/img/logo.png'}, 
+      {'image': 'assets/img/logo.png'},
       {'image': 'assets/img/loupe.png'},
-      {'image': 'assets/img/search.png'}, 
+      {'image': 'assets/img/search.png'},
     ];
   }
 
   ngOnInit(): void {
-    
+
   }
 
 }
