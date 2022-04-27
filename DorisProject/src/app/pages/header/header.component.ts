816da6b22a096985a08b46f14fa9081116576cf3
DESKTOP-OOHTFCE\SII Group <nillson.castillo@siigroup.cl>
2022-04-27 10:51:52 -0400

    pendiente arreglos botones del banner.

--- DorisProject/src/app/pages/header/header.component.ts
+++ DorisProject/src/app/pages/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationStart} from '@angular/router';
+import {Observable} from 'rxjs';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -8,21 +10,62 @@ import {Router} from '@angular/router';
 })
 
 export class HeaderComponent implements OnInit {
+  stockRouter = '/stock';
+  test = '/stock';
   name = window.localStorage.getItem('username');
+  navStart: Observable<NavigationStart>;
 
-  constructor(private  router: Router ) {
-     }
+  constructor(private  router: Router) {
 
-  ngOnInit(): void {}
+    this.navStart = router.events.pipe(
+      filter(evt => evt instanceof NavigationStart)
+    ) as Observable<NavigationStart>;
+
+    this.navStart.subscribe(result => {
+        // console.log('Navigation Started!' + result.url)
+        switch (result.url) {
+          case '/stock':
+            this.stockRouter = result.url;
+            break;
+
+          case '/add-stock' :
+            this.stockRouter = result.url;
+            break;
+
+          case '/categorias' :
+            this.stockRouter = result.url;
+            break;
+
+          case '/addcategory' :
+            this.stockRouter = result.url;
+            break;
+            console.log('Navigation stock: ' + result.url)
+            this.stockRouter = result.url;
+            break;
+
+          // console.log('Navigation categorias: ' + result.url)
+          // this.stockRouter = result.url;
+          // break;
+          default:
+            console.log('Navigation default: ' + result.url)
+            this.stockRouter = '/test';
+            break;
+
+        }
+      }
+    );
+  }
+
+  ngOnInit(): void {
+  }
 
   gotoProducts() {
     this.router.navigate(['stock']);
-
   }
+
   gotoCategorys() {
     this.router.navigate(['categorias']);
   }
-
 
 }
 
